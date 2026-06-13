5c999dac4f1975a88808a8c1a3cab30c3660c56c
kira <67888720+koffydrop@users.noreply.github.com>
2026-06-12 23:50:42 -0300

    fix(overlay): use getSystem

--- modules/overlay.nix
+++ modules/overlay.nix
@@ -1,4 +1,13 @@
-{ self, ... }:
+{ getSystem, ... }:
 {
-  flake.overlays.default = _: p: self.packages.${p.stdenv.hostPlatform.system};
+  # modified from https://github.com/hercules-ci/flake-parts/blob/40ee120dcc2c170d1180aa59bafbf046bb950706/extras/easyOverlay.nix#L44
+  flake.overlays.default =
+    final: prev:
+    let
+      system =
+        prev.stdenv.hostPlatform.system or (prev.system
+          or (throw "Could not determine the `hostPlatform` of Nixpkgs. Was this overlay loaded as a Nixpkgs overlay, or was it loaded into something else?")
+        );
+    in
+    (getSystem system).packages;
 }
